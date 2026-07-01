6b40c124a0672e60735ca10909e2066a195b776a
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-07-01 20:48:57 +0530

    moore voting algorithm

--- practice1.java
+++ practice1.java
@@ -40,12 +40,29 @@ public class practice1 {
         return -1;
 
     }
+
+    public int mooreVotingAlgo(int[] nums){
+        int freq = 0,ans = 0;
+        int n = nums.length;
+        for(int i = 0;i<n;i++){
+            if(freq == 0){
+                ans = nums[i];
+            }
+            if(ans == nums[i]){
+                freq++;
+            }else{
+                freq--;
+            }
+
+        }
+        return ans;
+    }
      
 
 public static void main(String[] args){
-    int[] nums = {1,2,3,1,3,3,3,3};
+    int[] nums = {2,2,1,1,1,2,2};
     practice1 obj = new practice1();
-    int ans = obj.majorityElement1(nums);
+    int ans = obj.mooreVotingAlgo(nums);
     System.err.println(ans);
 
 
