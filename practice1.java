c41deb947d93a99ca3254d4ea9079cba7c10bac9
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-07-08 20:50:50 +0530

    max area

--- practice1.java
+++ practice1.java
@@ -105,16 +105,16 @@ public class practice1 {
         }
     }
 
-      public double myPow(double x, int n) {
+    public double myPow(double x, int n) {
         long binForm = n;
-        if(n<0){
-            x = 1/x;
+        if (n < 0) {
+            x = 1 / x;
             binForm = -binForm;
         }
 
         double ans = 1;
-        while(binForm > 0){
-            if(binForm % 2 == 1){
+        while (binForm > 0) {
+            if (binForm % 2 == 1) {
                 ans = ans * x;
             }
             x *= x;
@@ -123,45 +123,59 @@ public class practice1 {
         return ans;
     }
 
-     public int maxSubarray(int[] nums){
-        int currSum = 0,maxSum = Integer.MIN_VALUE;
+    public int maxSubarray(int[] nums) {
+        int currSum = 0, maxSum = Integer.MIN_VALUE;
 
-        for(int i = 0;i < nums.length;i++){
+        for (int i = 0; i < nums.length; i++) {
             currSum += nums[i];
             maxSum = Math.max(currSum, maxSum);
 
-            if(currSum < 0){
+            if (currSum < 0) {
                 currSum = 0;
             }
 
         }
         return maxSum;
 
-     }
+    }
 
-     public int maxArea(int[] height){  //TLE Brute-Force-Approach
-            int maxWater = 0;
-            for(int i = 0;i<height.length;i++){
-                for(int j = i+1;j<height.length;j++){
-                    int wt = j-i;
-                    int ht = Math.min(height[i],height[j]);
-                    int area = wt*ht;
+    public int maxArea(int[] height) { // TLE Brute-Force-Approach
+        int maxWater = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                int wt = j - i;
+                int ht = Math.min(height[i], height[j]);
+                int area = wt * ht;
 
-                    maxWater = Math.max(maxWater,area);
-                }
+                maxWater = Math.max(maxWater, area);
             }
-            return maxWater;
-     }
+        }
+        return maxWater;
+    }
 
-    
+    public int maxArea1(int[] height) {
+        int n = height.length;
+        int lp = 0, rp = n - 1, ans = 0, maxWater = 0;
+        while (lp < rp) {
+            int w = rp - lp;
+            int ht = Math.min(height[lp], height[rp]);
+            int currWt = w * ht;
+            maxWater = Math.max(maxWater, currWt);
 
-    
+            if (height[lp] < height[rp]) {
+                lp++;
+            } else {
+                rp--;
+            }
 
+        }
+        return maxWater;
+    }
 
     public static void main(String[] args) {
-         int[] height = {1,1};
+        int[] height = { 1, 1 };
         practice1 obj = new practice1();
-        System.err.println(obj.maxArea(height));
-        
+        System.err.println(obj.maxArea1(height));
+
     }
 }
